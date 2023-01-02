7562f772426a94e2330ab7770b9b5c26c8320ca1
Martin Karugaba <martinkarugaba21@gmail.com>
2023-01-02 18:40:25 +0300

    add mobile-nav structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-black': '#272A31',
+        'primary-orange': '#EC5242',
+        'primary-grey': '#D3D3D3',
+      },
+    },
   },
   plugins: [],
-}
+};
