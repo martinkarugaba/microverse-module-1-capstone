b86bbb7a650d553e74e8b66e257c0147cde670fb
Martin Karugaba <martinkarugaba21@gmail.com>
2023-01-04 09:51:15 +0300

    fix main program item positioning

--- js/mainPrograms.js
+++ js/mainPrograms.js
@@ -44,12 +44,12 @@ mainPrograms.forEach((item) => {
   const { icon, title, description } = item;
 
   mainProgramsSection.innerHTML += `
-    <div class='project flex justify-between items-center w-[100%] h-auto px-4 py-6'>
-      <div class='w-[50%] flex justify-start items-center gap-x-1'>
-        <img class="h-[30px] w-[30px] object-contain" src=${icon} alt=${name}/>
-        <h3 class='text-lg font-medium text-primary-orange'>${title}</h3>
+    <div class='project flex justify-between items-center w-[100%] gap-x-2 h-auto px-4 py-6'>
+      <div class='w-[40%] h-[100%] flex justify-start items-center'>
+        <img class="h-[40px] w-[40px] object-contain" src=${icon} alt=${name}/>
+        <h3 class='text-lg font-semibold text-primary-orange ml-[5px]'>${title}</h3>
       </div>
-      <div class='grow ml-8 w[50%] h-[100%]'>
+      <div class='grow w-[60%] h-[100%]'>
         <p class='justify-self-end text-base leading-0 text-off-white'>${description}</p>
       </div>
     </div>
