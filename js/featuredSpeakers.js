c67ef478c9a91bccd9cc1666e7ccbd69cfb7c810
Martin Karugaba <martinkarugaba21@gmail.com>
2023-01-05 11:28:36 +0300

    add footer content

--- js/featuredSpeakers.js
+++ js/featuredSpeakers.js
@@ -69,10 +69,10 @@ featuredSpeakersList.forEach((speaker) => {
         <img class= 'speaker' src=${img} alt=${name}/>
       </div>
       <div class='w-[60%]'>
-        <h3 class='font-semibold'>${name}</h3>
+        <h3 class='font-semibold md:text-lg'>${name}</h3>
         <p class='text-sm text-primary-orange italic'>${jobTitle}</p>
         <div class="w-[30px] h-[2px] bg-primary-grey mt-2"></div>
-        <p class='text-sm mt-2'>${about}</p>
+        <p class='text-sm md:text-base mt-2'>${about}</p>
       </div>
     </article>
   `;
